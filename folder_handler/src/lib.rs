7f93b0c27591dfbebf22f3f7bc37986cdb43b679
Mayan <mayan.shoshani7@gmail.com>
2020-11-25 09:02:10 +0200

    Wrap in a module HandlersJson struct

--- folder_handler/src/lib.rs
+++ folder_handler/src/lib.rs
@@ -1,32 +1,36 @@
-use serde_json::Value;
-use crate::common_handlers::archive_join_handler::ArchiveJoinHandler;
-
 #[typetag::serde(tag = "type")]
 trait Handler {
     fn watch(&self); // Initialize handler to watch a folder
 }
 
-struct HandlersJson{
-    handlers: Vec<Value>
-}
+pub mod handlers_json {
+    use serde_json::Value;
+    use crate::common_handlers::archive_join_handler::ArchiveJoinHandler;
+    use crate::Handler;
 
-impl HandlersJson {
-    fn new() -> Self {
-        let archive_join_handler = ArchiveJoinHandler::default();
-        let dyn_archive_join_handler = &archive_join_handler as &dyn Handler;
-        let dyn_handlers = vec![dyn_archive_join_handler];
-        Self{
-            handlers: dyn_handlers.iter().map(|handler| {
-                let json = serde_json::to_string(handler).unwrap();
-                serde_json::from_str(&*json).unwrap()
-            }).collect()
+    pub struct HandlersJson{
+        pub(crate) handlers: Vec<Value>
+    }
+
+    impl HandlersJson {
+        pub fn new() -> Self {
+            let archive_join_handler = ArchiveJoinHandler::default();
+            let dyn_archive_join_handler = &archive_join_handler as &dyn Handler;
+            let dyn_handlers = vec![dyn_archive_join_handler];
+            Self{
+                handlers: dyn_handlers.iter().map(|handler| {
+                    let json = serde_json::to_string(handler).unwrap();
+                    serde_json::from_str(&*json).unwrap()
+                }).collect()
+            }
+        }
+
+        pub fn get_handler_types(&self) -> Vec<&str> {
+            self.handlers.iter().map(|handler| handler["type"].as_str().unwrap()).collect()
         }
     }
-
-    fn get_handler_types(&self) -> Vec<&str> {
-        self.handlers.iter().map(|handler| handler["type"].as_str().unwrap()).collect()
-    }
 }
+
 
 pub mod common_handlers;
 
@@ -35,11 +39,12 @@ mod tests {
     use std::fs;
     use std::any::{TypeId, Any};
     use crate::common_handlers::archive_join_handler::ArchiveJoinHandler;
-    use crate::HandlersJson;
+    use crate::handlers_json::HandlersJson;
 
     #[test]
     fn typetagging_works() {
         let handlers_json = HandlersJson::new();
+        println!("PageLoad json len: {}", handlers_json.get_handler_types().len());
         println!("PageLoad json: {}", handlers_json.handlers[0]);
         println!("PageLoad json: {}", handlers_json.handlers[0]["type"]);
     }
