b39efd64855806fb646d99185199cbbe7e605ec1
Mayan <mayan.shoshani7@gmail.com>
2020-12-21 09:13:27 +0200

    Require file on register subcommand

--- src/subcommand/register_subcommand.rs
+++ src/subcommand/register_subcommand.rs
@@ -21,11 +21,13 @@ impl SubCommandUtil for RegisterSubCommand {
             .about("Register handler to directory")
             .arg(Arg::with_name("debug").short("d")
                 .help("print debug information verbosely"))
-            .arg(Arg::with_name("handler_config").value_name("FILE").takes_value(true)
+            .arg(Arg::with_name("handler_config").value_name("FILE")
+                .takes_value(true).required(true)
                 .help("Handler configuration file"))
     }
 
     fn subcommand_runtime(&self, sub_matches: &ArgMatches) {
+        let handler_config_match = sub_matches.value_of("handler_config").unwrap();
         let path = env::current_dir().unwrap();
         println!("The current directory is {}", path.display());
         println!("{:?}", sub_matches);
