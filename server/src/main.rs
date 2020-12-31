7ce3db89386ce3a6cf1acf4721ca688cf83bc438
lambaaada <mayan.shoshani7@gmail.com>
2020-12-31 09:25:24 +0200

    Add cli basis to server application

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,19 @@
+use clap::{App, AppSettings, Arg};
+
+const DEFAULT_CONFIG_PATH: &str = "default.conf";
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("Folden Server")
+        .version("0.1")
+        .about("Folden background manager")
+        .setting(AppSettings::ColorAuto)
+        .arg(Arg::with_name("config")
+            .short("c")
+            .long("config")
+            .value_name("FILE")
+            .help("Sets a custom config file")
+            .takes_value(true));
+    let matches = app.get_matches();
+    let config = matches.value_of("config").unwrap_or(DEFAULT_CONFIG_PATH);
+    println!("Value for config: {}", config);
 }
