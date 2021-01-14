9c943676bfc7f735aec7a6f85e1e9f4ef4b9b7d9
lambaaada <mayan.shoshani7@gmail.com>
2021-01-14 12:02:21 +0200

    Add basis to server app run in cli

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,33 @@
-use clap::{App, AppSettings, Arg};
+use std::thread;
+use std::sync::mpsc;
+
+use clap::{App, AppSettings, Arg, SubCommand};
 
 const DEFAULT_CONFIG_PATH: &str = "default.conf";
+
+
+fn startup_server() {
+    //let (tx, rx) = mpsc::channel();
+    unimplemented!();
+}
 
 fn main() {
     let app = App::new("Folden Server")
         .version("0.1")
         .about("Folden background manager")
-        .setting(AppSettings::ColorAuto)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
         .arg(Arg::with_name("config")
             .short("c")
             .long("config")
             .value_name("FILE")
             .help("Sets a custom config file")
-            .takes_value(true));
+            .takes_value(true))
+        .subcommand(SubCommand::with_name("run")
+            .help("Startup Folden server"));
     let matches = app.get_matches();
     let config = matches.value_of("config").unwrap_or(DEFAULT_CONFIG_PATH);
     println!("Value for config: {}", config);
+    if let Some(_) = matches.subcommand_matches("run") {
+        startup_server();
+    }
 }
