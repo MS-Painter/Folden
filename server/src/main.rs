e1bd4a45ec3e46ed2d3df07308e361ddc2ec84c9
lambaaada <mayan.shoshani7@gmail.com>
2021-01-16 00:36:05 +0200

    Add inter process gRPC server runtime basis

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,48 @@
 use std::thread;
 use std::sync::mpsc;
+use std::net::{SocketAddr, IpAddr, Ipv4Addr};
 
 use clap::{App, AppSettings, Arg, SubCommand};
+use tonic::{Request, Response};
+use tonic::transport::Server as TonicServer;
+
+use generated_types::inter_process_server::{InterProcess, InterProcessServer};
+use generated_types::{RegisterToDirectoryRequest, RegisterToDirectoryResponse};
 
 const DEFAULT_CONFIG_PATH: &str = "default.conf";
 
+#[derive(Default)]
+struct Server {}
 
-fn startup_server() {
-    //let (tx, rx) = mpsc::channel();
-    unimplemented!();
+#[tonic::async_trait]
+impl InterProcess for Server {
+    async fn register_to_directory(&self,request:Request<RegisterToDirectoryRequest>,) ->
+    Result<Response<RegisterToDirectoryResponse>,tonic::Status> {
+        let request = request.into_inner();
+        println!("{}", request.full_directory_path);
+        println!("{}", request.handler_type_name);
+
+        Ok(Response::new(RegisterToDirectoryResponse{
+            message: "".to_string(),
+        }))
+    }
 }
 
-fn main() {
+async fn startup_server() -> Result<(), Box<dyn std::error::Error>> {
+    //let (tx, rx) = mpsc::channel();
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    let server = Server::default();
+
+    TonicServer::builder()
+        .add_service(InterProcessServer::new(server))
+        .serve(socket)
+        .await?;
+    Ok(())
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = App::new("Folden Server")
         .version("0.1")
         .about("Folden background manager")
@@ -28,6 +59,7 @@ fn main() {
     let config = matches.value_of("config").unwrap_or(DEFAULT_CONFIG_PATH);
     println!("Value for config: {}", config);
     if let Some(_) = matches.subcommand_matches("run") {
-        startup_server();
+        startup_server().await?;
     }
+    Ok(())
 }
