42fd166161acbf8746c7c945d7999cf47920eecb
lambaaada <mayan.shoshani7@gmail.com>
2021-02-02 11:04:25 +0200

    Add client register command logic

--- src/subcommand/register_subcommand.rs
+++ src/subcommand/register_subcommand.rs
@@ -1,7 +1,15 @@
+use std::{env, path::Path};
+
+use futures::executor::block_on;
+use tonic::transport::Channel;
+use tonic::transport::Error as TransportError;
+use clap::Error as CliError;
+use clap::{App, Arg, ArgMatches, ErrorKind};
+
 use folder_handler::handlers_json::HandlersJson;
+use generated_types::{RegisterToDirectoryRequest, inter_process_client::InterProcessClient};
+
 use crate::subcommand::subcommand::SubCommandUtil;
-use clap::{App, Arg, ArgMatches};
-use std::env;
 
 pub struct RegisterSubCommand {
     handlers_json: HandlersJson
@@ -21,15 +29,30 @@ impl SubCommandUtil for RegisterSubCommand {
             .about("Register handler to directory")
             .arg(Arg::with_name("debug").short("d")
                 .help("print debug information verbosely"))
+            .arg(RegisterSubCommand::construct_handler_arg("handler", &self.handlers_json))
             .arg(Arg::with_name("handler_config").value_name("FILE")
                 .takes_value(true).required(true)
                 .help("Handler configuration file"))
     }
 
-    fn subcommand_runtime(&self, sub_matches: &ArgMatches, client_connect_future: impl futures::Future<Output = Result<generated_types::inter_process_client::InterProcessClient<tonic::transport::Channel>, tonic::transport::Error>>) {
+    fn subcommand_runtime(&self, sub_matches: &ArgMatches, client_connect_future: impl futures::Future<Output = Result<InterProcessClient<Channel>, TransportError>>) {
         let handler_config_match = sub_matches.value_of("handler_config").unwrap();
+        let handler_config_path = Path::new(handler_config_match);
+        if !handler_config_path.exists() {
+            CliError::with_description("Config file doesn't exist", ErrorKind::InvalidValue).exit();
+        }
+
+        let handler_match = sub_matches.value_of("handler").unwrap();
         let path = env::current_dir().unwrap();
-        println!("The current directory is {}", path.display());
-        println!("{:?}", sub_matches);
+
+        let mut client = block_on(client_connect_future).unwrap();
+        let response = client.register_to_directory(RegisterToDirectoryRequest {
+            directory_path: String::from(path.as_os_str().to_str().unwrap()),
+            handler_type_name: handler_match.to_string(),
+            handler_config_path: handler_config_path.to_str().unwrap().to_string(),
+        });
+
+        let response = block_on(response).unwrap().into_inner();
+        println!("resp {:?}", response.message);
     }
 }
