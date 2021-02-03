73df70b4130fc4bdd0c5bf32660adc7695cc6a07
lambaaada <mayan.shoshani7@gmail.com>
2021-02-03 17:03:52 +0200

    Remove unused server config field db_path

--- server/src/config.rs
+++ server/src/config.rs
@@ -2,7 +2,6 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
-    pub db_path: String, // Local db file path
     pub continue_on_startup: bool, // On server startup, should continue working on previous handled directories? Saves this data to mapping file 
 }
 
