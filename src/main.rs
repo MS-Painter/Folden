e88cc5869918e84e63aeba11627696ff0ab5066d
lambaaada <mayan.shoshani7@gmail.com>
2021-02-08 14:37:12 +0200

    Remove list subcommand

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use crate::subcommand::subcommand::SubCommandUtil;
 use crate::subcommand::start_subcommand::StartSubCommand;
 use crate::subcommand::stop_subcommand::StopSubCommand;
 use crate::subcommand::status_subcommand::StatusSubCommand;
-use crate::subcommand::list_subcommand::ListSubCommand;
 use crate::subcommand::generate_subcommand::GenerateSubCommand;
 use crate::subcommand::register_subcommand::RegisterSubCommand;
 
@@ -26,13 +25,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let status_subcommand = StatusSubCommand::new(handlers_json.clone());
     let start_subcommand = StartSubCommand::new(handlers_json.clone());
     let stop_subcommand = StopSubCommand::new(handlers_json.clone());
-    let list_subcommand = ListSubCommand::new(handlers_json.clone());
     let gen_subcommand = GenerateSubCommand::new(handlers_json.clone());
     let app = App::new("Folden")
         .version("0.1")
         .about("System-wide folder event handling")
         .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(list_subcommand.construct_subcommand())
         .subcommand(start_subcommand.construct_subcommand())
         .subcommand(stop_subcommand.construct_subcommand())
         .subcommand(status_subcommand.construct_subcommand())
@@ -40,9 +37,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .subcommand(register_subcommand.construct_subcommand());
     let matches = app.get_matches();
 
-    if let Some(sub_matches) = list_subcommand.subcommand_matches(&matches) {
-        list_subcommand.subcommand_runtime(sub_matches, client_connect_future);
-    } else if let Some(sub_matches) = status_subcommand.subcommand_matches(&matches) {
+    if let Some(sub_matches) = status_subcommand.subcommand_matches(&matches) {
         status_subcommand.subcommand_runtime(sub_matches, client_connect_future);
     } else if let Some(sub_matches) = start_subcommand.subcommand_matches(&matches) {
         start_subcommand.subcommand_runtime(sub_matches, client_connect_future);
--- src/subcommand/mod.rs
+++ src/subcommand/mod.rs
@@ -2,6 +2,5 @@ pub mod subcommand;
 pub mod status_subcommand;
 pub mod start_subcommand;
 pub mod stop_subcommand;
-pub mod list_subcommand;
 pub mod generate_subcommand;
 pub mod register_subcommand;
