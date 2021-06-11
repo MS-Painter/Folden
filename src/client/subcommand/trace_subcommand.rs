b52eae423f2f52ec43fb0e16a327f36a5a03171c
STRONG-MAD <mayan.shoshani7@gmail.com>
2021-06-11 18:40:25 +0300

    Improved trace print with display of dir field only if not tracing specific dir

--- src/client/subcommand/trace_subcommand.rs
+++ src/client/subcommand/trace_subcommand.rs
@@ -35,21 +35,43 @@ impl SubCommandUtil for TraceSubCommand {
 
 fn execute_trace(sub_matches: &ArgMatches, mut client: HandlerServiceClient<tonic::transport::Channel>) {
     let mut directory_path = String::new();
-    if !sub_matches.is_present("all") {
+    let trace_all_directories = sub_matches.is_present("all");
+    if !trace_all_directories {
         let path = get_path_from_matches_or_current_path(sub_matches, "directory").unwrap();
         directory_path = path.into_os_string().into_string().unwrap();
     }
     let response = client.trace_handler(TraceHandlerRequest {
-        directory_path,
+        directory_path: directory_path.clone(),
     });
     let response = block_on(response);
     match response {
         Ok(response) => {
             let mut stream = response.into_inner();
             while let Some(response) = block_on(stream.message()).unwrap() {
-                println!("{:?}", response);
+                if trace_all_directories {
+                    print_response(response, true);
+                }
+                else if response.directory_path == directory_path {
+                    print_response(response, false);
+                }
             }
         }
         Err(e) => println!("{}", e)
+    }
+}
+
+fn print_response(response: generated_types::TraceHandlerResponse, print_directory: bool) {
+    if print_directory {
+        println!("
+        Directory - {}
+        Action - {}
+        Message - {}
+        ", response.directory_path, response.action.unwrap_or("None".to_string()), response.message);
+    }
+    else {
+        println!("
+        Action - {}
+        Message - {}
+        ", response.action.unwrap_or("None".to_string()), response.message);
     }
 }
